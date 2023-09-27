220b9ca548c8472b0989ac188198b5f07842be4e
nichoces <91145047+nichoces@users.noreply.github.com>
2023-09-27 11:20:12 +0200

    Delete 2_Data_Analysis/1-Numpy directory

--- 2_Data_Analysis/1-Numpy/2-Pandas/prueba.py
+++
@@ -1,5 +0,0 @@
-print("hola")
-
-lista = [1,2,3]
-for x in lista:
-    print(x*2)
--- 2_Data_Analysis/1-Numpy/2-Pandas/test.py
+++
@@ -1,7 +0,0 @@
-def imprime():
-    print("hola")
-
-def suma_numeros(num1, num2):
-    return num1+num2
-
-tamano = 10
