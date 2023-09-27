5d66ec31f8f8015effec599665034875282e0d26
nichoces <91145047+nichoces@users.noreply.github.com>
2023-09-27 11:23:00 +0200

    Add files via upload

---
+++ 2_Data_Analysis/2-Pandas/main.py
@@ -0,0 +1,20 @@
+import test
+test.imprime()
+print(test.tamano)
+print(test.suma_numeros(2,3))
+
+def lala(num):
+    res = num *3
+    return res+2
+
+
+num = lala(2)
+num2 = 3
+
+print("sí")
+
+res = num + num2
+print(res)
+
+
+
